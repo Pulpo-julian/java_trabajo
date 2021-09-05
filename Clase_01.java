ff1d8800f6ca31ca4b0d0d224f236dd279bf5805
Pulpo-julian <julian.schrodinger@gmail.com>
2021-09-05 13:59:55 -0500

    empiezo primer punto

---
+++ Clase_01.java
@@ -0,0 +1,23 @@
+
+public class Clase_01 {
+    // debo crear la clase principal 
+    public static void main(String[]args){
+        //declarar variables
+        int num1, num4, num5, num6;
+        String palabra;
+        float num2;
+        double num3;
+        
+        //para ingresar datos
+        
+        // opcion 1 dato quemado
+        num1= 3;
+        num4 = 5;
+        num6 = num1 * num4;
+        num5 = num1 + num4;
+        
+        // para imprimir por pantalla
+        
+        System.out.println("este  es un mensaje ");
+    }
+}
